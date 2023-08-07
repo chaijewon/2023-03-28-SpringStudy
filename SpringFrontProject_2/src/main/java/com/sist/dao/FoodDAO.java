73d39021e582462b9218ec0865759ae9cfdb0643
SIST <SIST@DESKTOP-08E10UE>
2023-08-07 18:52:15 +0900

    1111

--- SpringFrontProject_2/src/main/java/com/sist/dao/FoodDAO.java
+++ SpringFrontProject_2/src/main/java/com/sist/dao/FoodDAO.java
@@ -18,6 +18,7 @@ public class FoodDAO {
 		  +"FROM (SELECT fno,name,poster,score "
 		  +"FROM food_location ORDER BY fno ASC)) "
 		  +"WHERE num BETWEEN #{start} AND #{end}")*/
+   
    public List<FoodLocationVO> foodListData(Map map)
    {
 	   return mapper.foodListData(map);
