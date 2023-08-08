e843c12d61ea32541bd45a801140fa9226312dbe
SIST <SIST@DESKTOP-08E10UE>
2023-08-08 16:17:52 +0900

    12567

--- SpringFrontProject_3/src/main/java/com/sist/aop/GoodsAsepct.java
+++ SpringFrontProject_3/src/main/java/com/sist/aop/GoodsAsepct.java
@@ -1,5 +1,10 @@
 package com.sist.aop;
 import java.util.*;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+@Aspect
+@Component
 public class GoodsAsepct {
 
 }
